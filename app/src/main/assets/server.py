cc737e6dfad516e096e83e9cdfd93b576d0ecb34
Mariano <monzonmariano1@gmail.com>
2026-08-09 20:23:22 -0300

    feat: funcion para recibir http ping del cronjob y de el status 200 ok

--- app/src/main/assets/server.py
+++ app/src/main/assets/server.py
@@ -5,12 +5,25 @@ import uuid
 import random
 import string
 import os
+import http
 
 waiting_lines = {'5': [], '10': [], '15': [], '30': [], '60': []}
 rooms = {}
 player_to_room = {}
 player_names = {} 
 private_rooms = {} 
+
+
+# ---------------- FUNCION INTERCEPTORA ----------------------
+async def process_request(path, request_headers):
+    # Si alguien (como el cron job) hace un ping a la ruta principal o a /ping
+    if path == "/" or path == "/ping":
+        # Le devolvemos un 200 OK con un mensajito, sin iniciar WebSocket
+        return (http.HTTPStatus.OK, [], b"Raw Chess Server is awake!")
+    
+    # Si es otra ruta o el juego conectándose, retornamos None
+    # (Esto le dice a la librería: "Deja pasar la conexión, es un WebSocket")
+    return None
 
 def generate_room_code():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
@@ -96,7 +109,7 @@ async def chess_server(websocket):
                     rooms[room_id] = {'w': p1, 'b': p2}
                     player_to_room[p1] = room_id
                     player_to_room[p2] = room_id
-                    
+                    3. Agrégalo al inicio de tu servido
                     await p1.send(json.dumps({"type": "init", "color": "w", "opponent_name": player_names[p2], "time": match_time}))
                     await p2.send(json.dumps({"type": "init", "color": "b", "opponent_name": player_names[p1], "time": match_time}))
                 else:
@@ -146,7 +159,7 @@ async def chess_server(websocket):
 async def main():
     port = int(os.environ.get("PORT", 8001))
     
-    async with websockets.serve(chess_server, "0.0.0.0", port):
+    async with websockets.serve(chess_server, "0.0.0.0", port, process_request=process_request):
         print(f"Raw Chess Server 2.0 active on port {port}")
         await asyncio.Future()
 
